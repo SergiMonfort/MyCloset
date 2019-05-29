fc9d5e8274df9841aa2d32ee5c0ad6cacfa700e6
SergiMonfort <51057583+SergiMonfort@users.noreply.github.com>
2019-05-29 19:32:16 +0200

    Add files via upload

---
+++ ClaseCloset.java
@@ -0,0 +1,16 @@
+
+public class ClaseCloset {
+	private String closet = "salte del closet";
+
+	public ClaseCloset(String closet) {
+		this.closet = closet;
+	}
+
+	@Override
+	public String toString() {
+		return "Atrévetetete, " + closet;
+	}
+
+	
+
+}
